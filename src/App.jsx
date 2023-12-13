ebe3b40e55741d47b6c2d477d9a710670abac74a
Indries Andrei <indriesandrei@yahoo.co.uk>
2023-12-13 11:49:07 +0200

    Add React Router Package

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,9 @@ import ModeToggler from './components/ModeToggler'
 import Sidebar from './components/Sidebar'
 import MealsList from './components/MealsList'
 import Counter from './components/Counter'
+import { Link, Route, Routes } from 'react-router-dom'
+import Homepage from './Homepage'
+import AboutMe from './AboutMe'
 
 // we use useReducer and here we make the reducer (kind of useState with super powers)
 const reducer = (state, action) => {
@@ -31,6 +34,17 @@ function App() {
 
   return (
     <>
+      {/* React Router example */}
+      <nav>
+        <Link to="/">Homepage</Link>
+        <Link to="/about-me">About Me</Link>
+      </nav>
+      <Routes>
+        <Route path='/'  element={<Homepage />} />
+        <Route path='/about-me' element={<AboutMe />} />
+      </Routes>
+      {/* React Router example end */}
+
       <Header name="Anna" color="purple" />
       <Main greet="Howdy" />
       <Sidebar greet="Hi" />
@@ -40,18 +54,21 @@ function App() {
       <Child message={word + " at Little Lemon"} />
       <button onClick={handleClick}>Change text</button>
 
+
       {/* useReducer example */}
       <h1>Wallet: {state.money}</h1>
       <div>
         <button onClick={() => dispatch({type: 'ride'})}>A new customer!</button>
         <button onClick={() => dispatch({type: 'fuel'})}>Refill the tank!</button>
       </div>
+       {/* useReducer example end */}
 
       {/* Context API example */}
       <MealsProvider>
         <MealsList />
         <Counter />
       </MealsProvider>
+      {/* Context API example end */}
     </>
   )
 }
